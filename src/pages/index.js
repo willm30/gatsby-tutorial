378d6376fba0d79545318fd489613051a8976733
willm30 <64402732+willm30@users.noreply.github.com>
2021-10-08 20:22:16 +0200

    Finished p3 gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home'> 
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage 
+          src='https://zoolove.com.br/wp-content/uploads/2020/02/animal-2836987_1920_Easy-Resize.com_.jpg' 
+          alt='A beautiful Staffy'></StaticImage>
+        <StaticImage 
+          src='../images/helder.jpg' 
+          alt='Helder Nunes playing for Barcelona'></StaticImage>
     </Layout>
   )
 }
